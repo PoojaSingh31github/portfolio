ad06660480fb11a3b4a3e184e8ea2610943b891c
Pooja Singh <132251143+PoojaSingh31github@users.noreply.github.com>
2024-02-24 19:30:48 +0530

    Add files via upload

---
+++ custom.js
@@ -0,0 +1,10 @@
+// document.getElementById('downloadButton').addEventListener('click', function() {
+//     var blob = new Blob([document.documentElement.outerHTML], { type: 'application/pdf' });
+    
+//     var link = document.createElement('a');
+//     link.download = 'cv.pdf';
+//     link.href = window.URL.createObjectURL(blob);
+//     document.body.appendChild(link);
+//     link.click();
+//     document.body.removeChild(link);
+// });
